dc39661a414920dd87e0742475881c09856ddb57
Turkel <gadirzade@gmail.com>
2024-11-22 21:27:53 +0400

    dsaasd

--- apps/src/transfers.api/Controllers/LocalTransfer.cs
+++ apps/src/transfers.api/Controllers/LocalTransfer.cs
@@ -41,7 +41,7 @@ namespace Transfers.API.Controllers
                 // Deserialize JSON responses
                 var limitsResponse = JsonSerializer.Deserialize<LimitsResponse>(limitsTask.Result);
                 var accountsResponse = JsonSerializer.Deserialize<AccountsResponse>(accountsTask.Result);
-                var paymentsResponse = JsonSerializer.Deserialize<AccountsResponse>(paymentsTask.Result);
+                var paymentsResponse = JsonSerializer.Deserialize<string>(paymentsTask.Result);
 
                 // Combine results into a single object
                 var combinedResult = new
